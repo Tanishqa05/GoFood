446255eb4c5b35cd97be9816d0a802b87a6171d1
Tanishqa05 <tani05bandi@gmail.com>
2025-01-10 19:49:52 +0530

    2. added the carousel and editted the cards of home page.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,9 @@ import './App.css';
 import Home from './screens/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/js/bootstrap.bundle';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './screens/Login';
-
 function App() {
   return (
     <Router>
--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,40 +1,44 @@
-import React from 'react'
+import React from "react";
 
 export default function Card() {
   return (
     <div>
-            <div>
-        <div className="card mt-3" style={{"width": "18rem", "maxHeight": "360px"}}>
-          <img className="card-img-top" src="..." alt="Card image cap" />
+      <div>
+        <div
+          className="card mt-3"
+          style={{ width: "18rem", maxHeight: "450px" }}
+        >
+          <img className="card-img-top" src="/pizza.jpg" alt="Card image cap" />
           <div className="card-body">
             <h5 className="card-title">Card title</h5>
             <p className="card-text">
               Some quick example text to build on the card title and make up the
               bulk of the card's content.
             </p>
-            <div>
-              <div className="container w=100">
+            <div className="container p-0">
+              <div className="d-flex justify-content-between align-items-center">
                 <select className="m-2 h-100 bg-success rounded">
-                  {Array.from(Array(6), (e,i)=>{
-                    return(
-                      <option key={i+1} value={i+1}> {i+1} </option>
-                    )
+                  {Array.from(Array(6), (e, i) => {
+                    return (
+                      <option key={i + 1} value={i + 1}>
+                        {" "}
+                        {i + 1}{" "}
+                      </option>
+                    );
                   })}
-                  </select>
+                </select>
 
-                  <select className="m-2 h-100 bg-success rounded">
-                    <option value="half">Half</option>
-                    <option value="full">Full</option>
-                    </select>
+                <select className="m-2 h-100 bg-success rounded">
+                  <option value="half">Half</option>
+                  <option value="full">Full</option>
+                </select>
 
-                    <div className="fs-4 d-inline">
-                      Total price
-                    </div>
+                <div className="fs-4 m-2">Total price</div>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
