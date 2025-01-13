02d37e8c0e27e75c97d516373f5ceca4d0f63324
Tanishqa05 <tani05bandi@gmail.com>
2025-01-13 21:39:53 +0530

    retrieved thd data from the dbs ;created schema in the models folder; created creauser and inserted the user data manually using thunder client also

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,20 @@
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb+srv://tani05bandi:tani5mongo@cluster-1.4fqi6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-1';
+const mongoURI = 'mongodb+srv://tani05bandi:tani5mongo@cluster-1.4fqi6.mongodb.net/firstdb?retryWrites=true&w=majority&appName=Cluster-1';
 
 const mongoDB = async () => {
     try {
         await mongoose.connect(mongoURI);
         console.log("Connected to MongoDB");
+
+        // Fetch the food_items collection
+        const fetched_data = mongoose.connection.db.collection("food_items");
+
+        // Use `await` with the MongoDB query
+        const data = await fetched_data.find({}).toArray();
+        // console.log(data);
+
     } catch (err) {
-        console.log("Error connecting to MongoDB:", err);
+        console.error("Error connecting to MongoDB:", err);
     }
 };
 
